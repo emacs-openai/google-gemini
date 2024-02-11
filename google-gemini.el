37b790f069bb4005a02798f511753878faab907a
JenChieh <jcs090218@gmail.com>
2024-02-11 03:46:27 -0800

    Init project

---
+++ google-gemini.el
@@ -0,0 +1,37 @@
+;;; google-gemini.el --- Elisp library for the Google Gemini API  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  JenChieh
+
+;; Author: JenChieh <jcs090218@gmail.com>
+;; Maintainer: JenChieh <jcs090218@gmail.com>
+;; URL: https://github.com/emacs-openai/google-gemini
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: comm google gemini
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Elisp library for the Google Gemini API
+;;
+
+;;; Code:
+
+;; Happy coding! ;)
+
+(provide 'google-gemini)
+;;; google-gemini.el ends here
